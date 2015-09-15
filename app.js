d862de9d40187b462b0b678e1e0a6ad46c0eb0e8
Aaron <ufozrl@163.com>
2015-09-15 15:50:41 +0800

    test

--- app.js
+++ app.js
@@ -23,42 +23,25 @@ var config = {
 };
 
 app.use(express.query());
-app.use('/wechat', wechat(config, function (req, res, next) {
-  // 微信输入信息都在req.weixin上
-  var message = req.weixin;
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
-  } else {
-    // 回复高富帅(图文回复)
-    res.reply([
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ]);
-  }
-}));
+app.use('/wechat', wechat(config, weixin.text(function (message, req, res, next) {
+  res.reply('text');
+}).image(function (message, req, res, next) {
+  res.reply('image');
+}).voice(function (message, req, res, next) {
+  res.reply('voice');
+}).video(function (message, req, res, next) {
+  res.reply('video');
+}).location(function (message, req, res, next) {
+  res.reply('location');
+}).link(function (message, req, res, next) {
+  res.reply('link');
+}).event(function (message, req, res, next) {
+  res.reply('event');
+}).device_text(function (message, req, res, next) {
+  res.reply('device_text');
+}).device_event(function (message, req, res, next) {
+  res.reply('device_event');
+})));
 
 
 module.exports = app;
