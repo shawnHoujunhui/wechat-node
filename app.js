6286a096de59c6d329d64c1a80504a9bcb183f61
Aaron <ufozrl@163.com>
2015-09-15 14:59:16 +0800

    test

--- app.js
+++ app.js
@@ -22,62 +22,50 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+//app.use('/', routes);
+//app.use('/users', users);
+//
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  var err = new Error('Not Found');
+//  err.status = 404;
+//  next(err);
+//});
+//
+//// error handlers
+//
+//// development error handler
+//// will print stacktrace
+//if (app.get('env') === 'development') {
+//  app.use(function(err, req, res, next) {
+//    res.status(err.status || 500);
+//    res.render('error', {
+//      message: err.message,
+//      error: err
+//    });
+//  });
+//}
+//
+//// production error handler
+//// no stacktraces leaked to user
+//app.use(function(err, req, res, next) {
+//  res.status(err.status || 500);
+//  res.render('error', {
+//    message: err.message,
+//    error: {}
+//  });
+//});
 
 //wechat
 var wechat = require('wechat');
 var config = {
   token: 'wechat-node',
   appid: 'wxce1fff14f1989d01',
-  encodingAESKey: 'encodinAESKey'
 };
 
 app.use(express.query());
 app.use('/wechat', wechat('wechat-node').text(function (message, req, res, next) {
-  res.reply({
-    type: "text",
-    content: 'Hello world!'
-  });
-  //res.reply([
-  //  {
-  //    title: 'title',
-  //    description: 'description',
-  //    picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-  //    url: 'http://nodeapi.cloudfoundry.com/'
-  //  }
-  //]);
+  res.reply('text');
 }).image(function (message, req, res, next) {
   res.reply('image');
 }).voice(function (message, req, res, next) {
