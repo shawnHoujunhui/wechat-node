c68f2980d966613256dd51e29ea1ecf7fe2e72bf
Aaron <ufozrl@163.com>
2015-09-15 16:23:25 +0800

    session

--- app.js
+++ app.js
@@ -24,8 +24,13 @@ var config = {
 
 app.use(express.query());
 
+//被动调用
 app.use('/wechat', wechat(config, wechat.text(function (message, req, res, next) {
-  res.reply('text');
+  if(message.Content === 'list'){
+    res.wait('view');
+  }else{
+    res.replay('text');
+  }
 }).image(function (message, req, res, next) {
   res.reply('image');
 }).voice(function (message, req, res, next) {
@@ -41,9 +46,25 @@ app.use('/wechat', wechat(config, wechat.text(function (message, req, res, next)
 }).device_text(function (message, req, res, next) {
   res.reply('device_text');
 }).device_event(function (message, req, res, next) {
-  res.reply('device_event');
+  if (message.Event === 'subscribe_status' || message.Event === 'unsubscribe_status') {
+    res.reply("订阅事件");
+  } else {
+    res.reply('自定义菜单点击事件')
+  }
 })));
 
+var List = require('wechat').List;
+List.add('view', [
+  ['回复{a}查看我的性别', function (info, req, res) {
+    res.reply('我是个妹纸哟');
+  }],
+  ['回复{b}查看我的年龄', function (info, req, res) {
+    res.reply('我今年18岁');
+  }],
+  ['回复{c}查看我的性取向', '这样的事情怎么好意思告诉你啦- -']
+]);
+
+//主动调用
 var WechatAPI = require('wechat-api');
 
 var api = new WechatAPI('wx86d16afcfdc1afc6', 'bce57bdc8b5f4ab2efda5523d32d1e72');
